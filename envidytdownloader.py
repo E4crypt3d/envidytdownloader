2f1e9c0520ea2d09192ac83243ba2e0dd6310206
E4crypt3d <71579244+E4crypt3d@users.noreply.github.com>
2021-01-21 11:33:29 +0500

    Add files via upload

---
+++ envidytdownloader.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+import pytube
+from pytube import YouTube
+print('''               EnVideoYT DOWNLOADER
+                        ❦ᴱⁿⱽⁱᵈʸᵀᴰᵂ
+                Created by @E4crypt3d''')
+def downloadVideo():
+	URL = input("Paste the URL of the video: ")
+	if type(URL) == str:
+		yt = YouTube(URL)																										
+		video = yt.streams.filter(resolution='720p').first()
+		print("Video Title: == ",video.title)
+		print("Video Size: == ",video.filesize)
+		savepath = input("Please specify the directory you want to save the video: ")
+		if type(savepath) == str:
+			video.download(savepath)
+			print(video)
+			print("Video downloaded successfuly")
+		else: 
+			print("Please enter a vaid path")
+			downloadVideo()
+
+	else:
+		print("Please enter a valid URL")
+		downloadVideo()
+
+downloadVideo()
